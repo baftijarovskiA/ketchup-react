aca2de2ce653e165831d5ff84bb5abaa46100c77
Albin Baftijarovski <baftijarovski.a@outlook.com>
2021-04-10 22:37:38 +0200

    Footer component created

--- src/components/Account/index.jsx
+++ src/components/Account/index.jsx
@@ -1,11 +1,13 @@
 import React, {Component} from 'react';
 import './styles.css';
+import Footer from "../Footer";
 
 class Account extends Component{
     render() {
         return(
             <div className='account-component' >
                 <h4>Account component</h4>
+                <Footer />
             </div>
         )
     }
---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles.css';
+
+const Footer = () => {
+    return(
+        <div className='footer-component' >
+            <h4>Footer component</h4>
+        </div>
+    )
+}
+
+export default Footer;
