f038fd083af2425e937f03b2aaf92a0ecf4b995b
Albin Baftijarovski <baftijarovski.a@outlook.com>
2021-04-10 22:54:14 +0200

    Not found page done

--- src/components/NotFound/index.jsx
+++ src/components/NotFound/index.jsx
@@ -1,11 +1,18 @@
 import React, {Component} from 'react';
 import './styles.css';
+import Logo from '../../assets/logo_default.png';
+import {Link} from "react-router-dom";
 
 class NotFound extends Component{
     render() {
         return(
             <div className='not-found-component' >
-                <h4>Not found component</h4>
+                <div>
+                    <img src={Logo} alt="Ketch Up Logo" />
+                    <h1>Not found</h1>
+                    <h5>The page you are looking for is not found, <br/>or you are not authorized.</h5>
+                    <Link to='/'>Back to home</Link>
+                </div>
             </div>
         )
     }
